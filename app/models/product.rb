01aadddeaa08400d6838d60ac9cfbfb9c30129bb
Emmy Morris <emmymorris1102@gmail.com>
2021-11-30 13:18:44 -0700

    Update models to have relationships

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,7 @@
 class Product < ApplicationRecord
+  belongs_to :user
+
   validates :name, presence: true
   validates :quantity, presence: true, numericality: true
+  validates :user_id, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
   has_secure_password
+
+  has_many :products
   validates :email, presence: true, uniqueness: true
 end
