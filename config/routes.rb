9de5075b2b99c555f23e01766dbd519f5dcfe03c
Emmy Morris <emmymorris1102@gmail.com>
2021-11-30 16:50:17 -0700

    Fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,18 @@ Rails.application.routes.draw do
   root 'welcome#index'
 
   resources :users
-  resources :products
+  # resources :products
   resources :sessions, only: [:new, :create, :destroy]
 
-  get '/signup', to: 'users#new', as: 'signup'
-  get '/login', to: 'sessions#new', as: 'login'
-  get '/logout', to: 'sessions#destroy', as: 'logout'
+  scope module: :users do
+     resources :products
+  end
+
+  get '/register', to: 'users#new'
+  get '/dashboard', to: 'users#show'
+
+   get '/login', to: 'sessions#new'
+   post '/login', to: 'sessions#create'
+   delete '/logout', to: 'sessions#destroy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
