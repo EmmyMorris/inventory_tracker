76c31a0ae910ece6ec85dcd9975ecd889916b39f
Emmy Morris <emmymorris1102@gmail.com>
2021-12-01 15:10:33 -0700

    Fix delete function

--- config/routes.rb
+++ config/routes.rb
@@ -2,18 +2,17 @@ Rails.application.routes.draw do
   root 'welcome#index'
 
   resources :users
-  # resources :products
   resources :sessions, only: [:new, :create, :destroy]
 
   scope module: :users do
-     resources :products
+    resources :products
   end
 
   get '/register', to: 'users#new'
   get '/dashboard', to: 'users#show'
 
-   get '/login', to: 'sessions#new'
-   post '/login', to: 'sessions#create'
-   delete '/logout', to: 'sessions#destroy'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
